9f509309837c8ec5572f76887950f4a04d8760fe
sachin <sachinbijawad01@gmail.com>
2024-01-14 19:00:04 +0530

    Architect changed in branch

--- src/Interview/Program2.java
+++ src/Interview/Program2.java
@@ -1,6 +1,10 @@
 package Interview;
 
 public class Program2 {
+	
+	public static void m1() {
+		System.out.println(" adding the method1");
+	}
 	public static void main(String[] args) {
 		System.out.println(" this is the demo for the git ");
 	}
